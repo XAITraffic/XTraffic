fc39d4721838b968202f49faba670b75d97edaac
Xiaochuan Gou <goux@vsc509-29-r.ibex.kaust.edu.sa>
2025-08-01 15:07:38 +0300

    update collection

--- process/fetch_url_list.py
+++ process/fetch_url_list.py
@@ -9,14 +9,14 @@ import os
 district_id = 202
 
 
-district_id_list = [3, 4,5,6,7,8,10,11,12]
-session_id = "24c47b3ff255b9a0b97fc07855b2924f"
+district_id_list = []
+session_id = ""
 session = requests.Session()
 session.cookies.set("PHPSESSID", session_id, domain="pems.dot.ca.gov")
 
 
 def save_file_ex(data, file_name):
-    data_dir = 'raw_data/year_2022'
+    data_dir = ''
     data_path = os.path.join(data_dir, file_name)
     data_file = open(data_path, "w")
     data_file.write(data)
